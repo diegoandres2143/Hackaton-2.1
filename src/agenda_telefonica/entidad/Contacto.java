1d4f4c14e1fd5b842a0afbe1ecf8643062be5d7c
Samuel Lopez <samuellopez01@hotmail.com>
2025-09-03 11:01:50 -0500

    Validación usuario vacio

--- src/agenda_telefonica/entidad/Contacto.java
+++ src/agenda_telefonica/entidad/Contacto.java
@@ -26,6 +26,9 @@ public class Contacto {
     }
 
     public void setNombre(String nombre) {
+        if(nombre == null || nombre.isEmpty()){
+            throw new IllegalArgumentException("El nombre no puede estar vacio");
+        }
         this.nombre = nombre;
     }
 
@@ -37,5 +40,12 @@ public class Contacto {
         this.telefono = telefono;
     }
 
-
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono=" + telefono +
+                '}';
+    }
 }
--- src/agenda_telefonica/entidad/Main.java
+++ src/agenda_telefonica/entidad/Main.java
@@ -3,5 +3,11 @@ package agenda_telefonica.entidad;
 public class Main {
     public static void main(String[] args) {
 
+        Contacto contacto = new Contacto();
+        System.out.println(contacto.toString());
+
+        Contacto contacto2 = new Contacto();
+        contacto2.setNombre("");
+        System.out.println(contacto2.toString());
     }
 }
