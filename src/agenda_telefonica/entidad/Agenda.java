e420257e1f2a2f0cad0f461e678a7a72d8347787
Diego Andrés Candamil Puerta <diegoandres2143@gmail.com>
2025-09-03 11:00:05 -0500

    Se agrega el inicio de agenda

--- src/agenda_telefonica/entidad/Agenda.java
+++ src/agenda_telefonica/entidad/Agenda.java
@@ -1,4 +1,25 @@
 package agenda_telefonica.entidad;
 
+import java.util.ArrayList;
+
 public class Agenda {
+    // Lista de contactos (atributo de la clase)
+    private ArrayList<Contacto> contactos;
+
+    // Constructor: inicializa la lista
+    public Agenda() {
+        contactos = new ArrayList<>();
+    }
+
+    // Método para agregar un contacto
+    public void add(Contacto c) {
+        contactos.add(c);
+    }
+
+    // Método para mostrar todos los contactos
+    public void mostrarContactos() {
+        for (Contacto c : contactos) {
+            System.out.println(c);
+        }
+    }
 }
